3e1c867c5422dae7635c69600a96a7567e7bb21c
archylex <archylex@protonmail.com>
2020-09-28 23:46:16 +0300

    feat: get solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n).toString();
+    
+    let res = [];    
+    
+    for (let i = n.length - 1; i >= 0; i--)
+        res.push(n[i]);
+    
+    return Number(res.join``);
 }
